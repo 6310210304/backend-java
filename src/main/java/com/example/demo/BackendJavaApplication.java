1b9a5bea3a30e191fff9432949e9550ebeaec5bb
6310210304 <6310210304@psu.ac.th>
2023-07-18 20:24:12 +0700

    lab2new getAll,create Employee

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.log.SystemLogHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,8 @@ public class BackendJavaApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(BackendJavaApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello spring");
+		System.out.println("Spring boot");
 	}
 
 }
